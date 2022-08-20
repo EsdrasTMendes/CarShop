0a86d62bd0dbb5721673e031e75b9f4d2e416191
Esdras <esdrastmendes@gmail.com>
2022-08-19 21:11:05 -0300

    implementação do type  IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const vehycleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().min(1900).max(2022),
+  color: z.string({
+    required_error: 'Color is required',
+    invalid_type_error: 'Color must be a string',
+  }).min(3, { message: 'Color must be 3 or more characters long' }),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof vehycleZodSchema>;
+
+export { vehycleZodSchema };
