24ef0041c23676f926bef9f0a79399fc9a1e550b
Esdras <esdrastmendes@gmail.com>
2022-08-22 00:30:35 -0300

    feat: cria as rotas get by id  e get all

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -9,4 +9,14 @@ export default class CarController {
     const result = await this._service.create(req.body);
     return res.status(201).json(result);
   }
+
+  public async getAll(req: Request, res: Response<ICar[]>) {
+    const result = await this._service.read();
+    return res.status(200).json(result);
+  }
+
+  public async getOne(req: Request, res: Response<ICar>) {
+    const result = await this._service.readOne(req.params.id);
+    return res.status(200).json(result);
+  }
 }
--- src/services/car.service.ts
+++ src/services/car.service.ts
@@ -1,6 +1,7 @@
 import IService from '../interfaces/IService';
 import { ICar, zodCar } from '../interfaces/ICar';
 import { IModel } from '../interfaces/IModel';
+import { ErrorTypes } from '../errors/catalog';
 
 class CarService implements IService<ICar> {
   private _car:IModel<ICar>;
@@ -14,6 +15,18 @@ class CarService implements IService<ICar> {
       throw parsed.error;
     }
     return this._car.create(obj);
+  }
+
+  public async read():Promise<ICar[]> {
+    const car = await this._car.read();
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
+  }
+
+  public async readOne(_id: string):Promise<ICar> {
+    const car = await this._car.readOne(_id);
+    if (!car) throw new Error(ErrorTypes.EntityNotFound);
+    return car;
   }
 }
 
